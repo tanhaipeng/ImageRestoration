7174564b520692385641844f05f445e71b808a91
tanhp <sd12345.love@163.com>
2015-05-11 15:53:32 +0800

    添加opencv支持

--- matlab2cpp/matlab2cpp/deblur.cpp
+++ matlab2cpp/matlab2cpp/deblur.cpp
@@ -1,7 +1,22 @@
 #include "deblur.h"
 
 
-void deblur( double **input,double **output,int height,int width,int scale,int iter,int psf )
+void deblur( double **input,double **output,int height,int width,int ms,int iter,int psf )
+{
+	double scale;
+	double **simg;
+	// multi-scale iteration
+	for(int i=0;i<ms;i++)
+	{
+		scale=1.0/ms; 
+		//imresize(input,simg,scale);				// image resize
+
+
+	}
+}
+
+
+void imresize( double **input,double **&simg,double scale )
 {
 
 }
--- matlab2cpp/matlab2cpp/deblur.h
+++ matlab2cpp/matlab2cpp/deblur.h
@@ -1,11 +1,19 @@
 
+
 // deblur main function
 // input: blur image
 // output: clear image
 // height: image height
 // width: image width
-// scale: multi-scale number
+// ms: multi-scale number
 // iter: iteration number
 // psf: blur kernel size
 
-void deblur(double **input,double **output,int height,int width,int scale,int iter,int psf);
+void deblur(double **input,double **output,int height,int width,int ms,int iter,int psf);
+
+
+// image resize main fun
+// input: image
+// simg: resize image
+// scale: 
+void imresize(double **input,double **&simg,double scale);
